967adc5d5681c01911676b348dae28690ba35ea6
marsade <adenijifara@gmail.com>
2023-09-20 03:50:55 -0700

    create file creation error msg

--- monty.c
+++ monty.c
@@ -7,11 +7,20 @@
  */
 int main(int argc, UNUSED char *argv[])
 {
-	int fd UNUSED;
+	FILE *file;
+	char *fn;
+	char buffer[1024] UNUSED;
 
 	if (argc != 2)
 	{
 		perror("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	fn = argv[1];
+	file = fopen(fn, "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", fn);
 		exit(EXIT_FAILURE);
 	}
 	return (0);
