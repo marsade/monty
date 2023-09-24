00f322ceb3a42a16b77c0d9390234c7b7b666f94
marsade <adenijifara@gmail.com>
2023-09-24 20:07:15 +0300

    fix ascii bug

--- pstr.c
+++ pstr.c
@@ -16,8 +16,8 @@ void f_pstr(stack_t **head, UNUSED unsigned int counter)
 	{
 		if (h->n == 0)
 			break;
-		else if ((h->n < 65) && (h->n > 122))
-			continue;
+		else if ((h->n < 65) || (h->n > 122))
+			break;
 		putchar(h->n);
 		h = h->next;
 	}
