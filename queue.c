b4694ce6b75ff8eb5da15e6a4b9a7c4b770983e7
marsade <adenijifara@gmail.com>
2023-09-24 19:59:21 +0300

    minot change

--- queue.c
+++ queue.c
@@ -1,18 +1,5 @@
 #include "monty.h"
 /**
- * f_queue - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_queue(stack_t **head, unsigned int counter)
-{
-	(void)head;
-	(void)counter;
-	bus.lifi = 1;
-}
-
-/**
  * addqueue - add node to the tail stack
  * @n: new_value
  * @head: head of the stack
