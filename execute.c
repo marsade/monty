2b6a8577fee2c672228de4521b8a980fe78497db
marsade <adenijifara@gmail.com>
2023-09-24 18:25:33 +0300

    task 15 complete

--- execute.c
+++ execute.c
@@ -3,11 +3,10 @@
 * execute - executes the opcode
 * @stack: head linked list - stack
 * @counter: line_counter
-* @file: poiner to monty file
 * @content: line content
 * Return: no return
 */
-int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
+void execute(char *content, stack_t **stack, unsigned int counter)
 {
 	instruction_t opst[] = {
 				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
@@ -20,27 +19,40 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 				{"mul", f_mul},
 				{"mod", f_mod},
 				{"pchar", f_pchar},
+				{NULL, NULL}
 				};
 	unsigned int i = 0;
 	char *op;
 
 	op = strtok(content, " \n\t");
 	if (op && op[0] == '#')
-		return (0);
+		return;
 	bus.arg = strtok(NULL, " \n\t");
 	while (opst[i].opcode && op)
 	{
 		if (strcmp(op, opst[i].opcode) == 0)
-		{	opst[i].f(stack, counter);
-			return (0);
+		{
+			opst[i].f(stack, counter);
+			return;
+		}
+		else if (strcmp(op, "queue") == 0)
+		{
+			bus.lifi = 1;
+			break;
+		}
+		else if (strcmp(op, "stack") == 0)
+		{
+			bus.lifi = 0;
+			break;
 		}
 		i++;
 	}
 	if (op && opst[i].opcode == NULL)
-	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
-		fclose(file);
+	{
+		fclose(bus.file);
+		fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
 		free(content);
 		free_stack(*stack);
-		exit(EXIT_FAILURE); }
-	return (1);
+		exit(EXIT_FAILURE);
+	}
 }
